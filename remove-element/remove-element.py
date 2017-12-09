5d6c5877e4a954c531cf8458e39d083aa8018cbc
leiz2192 <zhanglei2192@126.com>
2017-12-10 00:22:28 +0800

    add remove element

---
+++ remove-element/remove-element.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# coding=utf-8
+
+class Solution:
+    """
+    @param: A: A list of integers
+    @param: elem: An integer
+    @return: The new length after remove
+    """
+    def removeElement(self, A, elem):
+        if A is None: return 0,[]
+        if len(A) == 0: return 0,[]
+        while elem in A: A.remove(elem)
+        # result = [e for e in A if e != elem]
+        return len(A), A
+
+if __name__ == '__main__':
+    A = [0,4,4,0,4,4,4,0,2]
+    elem = 4
+    sol = Solution()
+    print(sol.removeElement(A, elem))
+    print(sol.removeElement(None, elem))
+    print(sol.removeElement([], elem))
