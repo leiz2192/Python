c1fecf8f70f956b26ab96290235788d0aae09d70
leiz2192 <zhanglei2192@126.com>
2017-12-10 10:23:00 +0800

    add subarray-sum, remove-duplicates-from-sorted-array

---
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# coding = utf-8
+
+class Solution:
+    """
+    @param: nums: An ineger array
+    @return: An integer
+    """
+    def removeDuplicates(self, nums):
+        if nums is None: return 0
+        nums_len = len(nums)
+        if nums_len == 0: return 0
+        if nums_len == 1: return 1
+        # elem = nums[nums_len - 1]
+        # for i in range(nums_len - 2, -1, -1):
+        #     if nums[i] != elem: elem = nums[i]
+        #     else: nums.remove(elem)
+
+        last_index = 0
+        for i in range(1, nums_len):
+            if nums[i] != nums[last_index]:
+                last_index += 1
+                nums[last_index] = nums[i]
+        del nums[last_index + 1:nums_len]
+        print(nums)
+        return len(nums)
+
+if __name__ == '__main__':
+    nums = [1, 1, 2, 2]
+    sol = Solution()
+    print(sol.removeDuplicates(nums))
+    print(sol.removeDuplicates([1, 2]))
+
---
+++ subarray-sum/subarray-sum.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# coding = utf-8
+
+class Solution:
+    """
+    @param: nums: A list of integers
+    @return: A list of integers includes the index of the first number and the index of the last number
+    """
+    def subarraySum(self, nums):
+        if nums is None: return []
+        if nums is False: return []
+        if nums[0] == 0: return [0, 0]
+        sum_hash = {}
+        sum = 0
+        for i in range(len(nums)):
+            sum += nums[i]
+            if sum == 0: return [0, i]
+            if sum in sum_hash: return [sum_hash[sum] + 1, i]
+            sum_hash[sum] = i
+        return []
+
+if __name__ == '__main__':
+    nums = [-2, 1, 2, -3, 4]
+    sol = Solution()
+    print(sol.subarraySum(nums))
+    print(sol.subarraySum([0, -3]))
+
