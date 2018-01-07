8c77a0439ee5070b033ca56ddfa833c161b17ae2
leiz2192 <zhanglei2192@126.com>
2018-01-07 21:27:19 +0800

    rotate-bits-left

---
+++ rotate-bits-left/rotate-bits-left.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+class Solution:
+    """
+    @param: : a number
+    @param: : digit needed to be rorated
+    @return: a number
+    """
+
+    def leftRotate(self, n, d):
+        bin_str = bin(n)[2:]
+        print(bin_str)
+        supplement_num = 32 - len(bin_str)
+        supplement_str = ''.join(str(0) for i in range(supplement_num))
+        bin_str = supplement_str + bin_str
+        print(bin_str)
+        bin_str = bin_str[d:] + bin_str[:d]
+        return int(bin_str, 2)
+
+
+def main():
+    print(Solution().leftRotate(123, 4))
+    print(Solution().leftRotate(255, 1))
+
+if __name__ == '__main__':
+    main()
