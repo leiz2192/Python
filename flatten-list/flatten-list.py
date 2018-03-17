75e321b2dba69f08a1410596a6757519655713a1
zhanglei <zhanglei2192@126.com>
2018-03-17 22:52:04 +0800

    modify flatten-list

--- flatten-list/flatten-list.py
+++ flatten-list/flatten-list.py
@@ -2,27 +2,22 @@
 # -*- coding:utf-8 -*-
 
 
-import queue
-
-
 class Solution(object):
     # @param nestedList a list, each element in the list
     # can be a list or integer, for example [1,2,[1,2]]
     # @return {int[]} a list of integer
     def flatten(self, nestedList):
-        list_queue = queue.Queue()
-        for one_ele in nestedList:
-            list_queue.put(one_ele)
+        if isinstance(nestedList, int):
+            return [nestedList]
 
         result_list = []
-        while not list_queue.empty():
-            one_ele = list_queue.get()
-            if isinstance(one_ele, int):
-                result_list.append(one_ele)
-            elif isinstance(one_ele, list):
-                for one in one_ele:
-                    list_queue.put(one)
-
+        while nestedList:
+            one_element = nestedList.pop(0)
+            if isinstance(one_element, int):
+                result_list.append(one_element)
+            elif isinstance(one_element, list):
+                while one_element:
+                    nestedList.insert(0, one_element.pop())
         return result_list
 
 
@@ -30,6 +25,7 @@ def main():
     print(Solution().flatten([1, 2, [1, 2]]))
     print(Solution().flatten([4, [3, [2, [1]]]]))
     print(Solution().flatten([[1, 1], 2, [1, 1]]))
+    print(Solution().flatten(10))
 
 
 if __name__ == '__main__':
