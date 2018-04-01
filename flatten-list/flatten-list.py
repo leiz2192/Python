c203338b71a4bce2a8f051caddffb61ec56bad6e
zhanglei <zhanglei2192@126.com>
2018-04-01 17:10:25 +0800

    using yield to impl flatten list

--- flatten-list/flatten-list.py
+++ flatten-list/flatten-list.py
@@ -2,6 +2,17 @@
 # -*- coding:utf-8 -*-
 
 
+from collections import Iterable
+
+
+def iter_flatten(items, ignore_types=(str, bytes)):
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+            yield from iter_flatten(x)
+        else:
+            yield x
+
+
 class Solution(object):
     # @param nestedList a list, each element in the list
     # can be a list or integer, for example [1,2,[1,2]]
@@ -10,15 +21,17 @@ class Solution(object):
         if isinstance(nestedList, int):
             return [nestedList]
 
-        result_list = []
-        while nestedList:
-            one_element = nestedList.pop(0)
-            if isinstance(one_element, int):
-                result_list.append(one_element)
-            elif isinstance(one_element, list):
-                while one_element:
-                    nestedList.insert(0, one_element.pop())
-        return result_list
+        return [x for x in iter_flatten(nestedList)]
+
+        # result_list = []
+        # while nestedList:
+        #     one_element = nestedList.pop(0)
+        #     if isinstance(one_element, int):
+        #         result_list.append(one_element)
+        #     elif isinstance(one_element, list):
+        #         while one_element:
+        #             nestedList.insert(0, one_element.pop())
+        # return result_list
 
 
 def main():
